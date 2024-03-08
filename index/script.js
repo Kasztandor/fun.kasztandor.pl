cbd69a6ba476953818fab6b244795ebcd8e43696
kasztandor <toja@example.com>
2024-03-08 22:34:17 +0100

    Checking if device is mobile to diable pc/mobile only games

--- index/script.js
+++ index/script.js
@@ -1,3 +1,11 @@
+//check if device is mobile or pc
+
+if((typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1)){
+    document.body.classList.add("mobile");
+} else {
+    document.body.classList.add("pc");
+}
+
 fetch("games.json")
 .then((response) => response.json())
 .then((json) => {
@@ -6,7 +14,7 @@ fetch("games.json")
     Object.entries(infos).forEach(element=>
     {
         let el = element[1]
-        inner += `<div onclick="window.location.href='${element[0]}'" style="${el.color?"--color:"+el.color+";":""}${el.bgcolor?"--bgcolor:"+el.bgcolor+";":""}"><h1>${el.name}</h1><span>${el.description}</span></div>`
+        inner += `<div onclick="window.location.href='${element[0]}'" class="platform-${el.device}" style="${el.color?"--color:"+el.color+";":""}${el.bgcolor?"--bgcolor:"+el.bgcolor+";":""}"><h1>${el.name}</h1><span>${el.description}</span></div>`
     })
     document.querySelector("#games").innerHTML = inner
 })
